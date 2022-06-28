a3a5705ec8c06cc01a4a44a79fd6d0cb141f2c9c
kylest0kes <kylestokes1551@gmail.com>
2022-06-28 19:52:44 -0400

    set up the content in the side sections to be where they need to, to stay centered on resizing, and laidout more content

--- src/Sections/Left_Side/LeftSide.js
+++ src/Sections/Left_Side/LeftSide.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React from "react";
+import "./LeftSide.css";
 
-import './LeftSide.css';
+import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const LeftSide = () => {
   return (
-    <div className='ls-container'>
-      <ul className='social-list'>
-        <li className='social-li'><p>LinkedIn</p></li>
-        <li className='social-li'><p>Github</p></li>
-        <li className='social-li'><p>Email</p></li>
+    <div className="ls-container">
+      <ul className="social-list">
+        <li className="social-li">
+          <a
+            href="https://github.com/kylest0kes"
+            target="blank"
+            className="github-icon"
+          ><FontAwesomeIcon className="social-icons" icon={faGithubSquare} /></a>
+        </li>
+        <li className="social-li">
+          <a
+            href="https://www.linkedin.com/in/kylest0kes/"
+            target="blank"
+            className="linkedin-icon"
+          ><FontAwesomeIcon className="social-icons" icon={faLinkedin} /></a>
+        </li>
+        <li className="social-li">
+          <a
+            href="mailto: kylestokes1551@gmail.com"
+            target="blank"
+            className="email-icon"
+          ><FontAwesomeIcon className="social-icons" icon={faEnvelope} /></a>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default LeftSide;
