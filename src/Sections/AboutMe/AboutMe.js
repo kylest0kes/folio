a2c415afc1563461f7218ef9700305457f810c03
kylest0kes <kylestokes1551@gmail.com>
2022-09-12 19:20:03 -0400

    created the skeleton for the about me section

--- src/Sections/AboutMe/AboutMe.js
+++ src/Sections/AboutMe/AboutMe.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import kyle from "../../Assets/imgs/kyle.jpg";
 
-import './AboutMe.css';
+import "./AboutMe.css";
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
-  )
-}
+    <div className="bio-container">
+          <h1 className="bio-header">1. About Me----------------</h1>
+      <div class="bio-card">
+        <div class="bio-card-text">
+          <span class="about-me-desc">
+           Hello there! My name is Kyle and I blah blah blah blah blah skljdf asdf fd fdfasd fasdf 
+           <br/><br/>
+           Blah blah
+           <br/><br/>
+           Tech I have been working with recently:
+           
+          </span>
+        </div>
+
+        <div class="bio-card-img">
+          <img src={kyle} class="img-responsive" alt="kyle" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutMe;
