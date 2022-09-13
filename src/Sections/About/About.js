063fec43e8d827f5deb943ceeb2d9202877d749c
kylest0kes <kylestokes1551@gmail.com>
2022-09-13 19:51:32 -0400

    filled in the about me section

--- src/Sections/About/About.js
+++ src/Sections/About/About.js
@@ -11,12 +11,12 @@ const About = () => {
           <h1 className="name">Kyle Stokes.</h1>
           <h1 className="below-name">I build things with code.</h1>
           <div className="about-text-sec">
-            <h3 className="about-text">
+            <p className="about-text">
               I am a software developer who specializes in building high quality,
               seamless online products. Currently I spend my time transforming the digital 
               experiences of the financial services industry with 
               <a className="capco" target='blank' href="https://www.capco.com">Capco</a>.
-            </h3>
+            </p>
           </div>
           <br/>
           <a className="about-btn" href="=">Check Out My Work</a>
--- src/Sections/AboutMe/AboutMe.js
+++ src/Sections/AboutMe/AboutMe.js
@@ -6,16 +6,37 @@ import "./AboutMe.css";
 const AboutMe = () => {
   return (
     <div className="bio-container">
-          <h1 className="bio-header">1. About Me...</h1>
+      <h1 className="bio-header">1. About Me...</h1>
       <div class="bio-card">
         <div class="bio-card-text">
           <span class="about-me-desc">
-           Hello and welcome! My name is Kyle and I blah blah blah blah blah skljdf asdf fd fdfasd fasdf 
-           <br/><br/>
-           Blah blah
-           <br/><br/>
-           Tech I have been working with recently:
-
+            Hey there! My name is Kyle and I live to create things that live in
+            the digital space. My interest in web development started way back
+            in 2007 when I began tinkering with MySpace layouts and themes. This
+            taught me a whole lot about the basics in web development.
+            <br />
+            <br />
+            Jump ahead to present day, and I’ve had the privilege of learning
+            Fullstack Web Development at the University of Central Florida,
+            learning a multitude of different technologies through Udemy, as
+            well as working at a major player in the financial services
+            industry. My primary focus currently is building accessible,
+            inclusive products and digital experiences at Capco for a variety of
+            financial clients.
+            <br />
+            <br />
+            Tech I have been working with recently:
+            <br/>
+            <ul className="tech">
+              <li>React</li>
+              <li>Angular</li>
+              <li>Typescript</li>
+              <li>Python</li>
+              <li>Node.js</li>
+              <li>Drupal</li>
+              <li>Javascript (ES6+)</li>
+              <li>Matplotlib</li>
+            </ul>
           </span>
         </div>
 
