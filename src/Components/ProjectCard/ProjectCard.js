49496160be278d9f4ef0344f6b577ded6eaf072b
kylest0kes <kylestokes1551@gmail.com>
2024-06-07 17:43:59 -0400

    style updates to the project card component to make it more uniform

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -4,19 +4,21 @@ import "./ProjectCard.css";
 const ProjectCard = (props) => {
   return (
     <div class="card-container">
-    <div class="project-card">
-      <div class="card-project-preview">
-      </div>
-      <div class ="card-project-content">
-        <h4 class="card-project-title">{props.title}</h4>
-        <p class="card-project-description">{props.desc}</p>
-        <button class="project-btn">View {props.title}</button>
-        <div className="lang-icons">
-          {props.language}
+      <div class="project-card">
+        <div class="card-project-preview">
+        </div>
+        <div class ="card-project-content">
+          <h4 class="card-project-title">{props.title}</h4>
+          <p class="card-project-description">{props.desc}</p>
+          <div className="bottom-content">
+            <button class="project-btn">View {props.title}</button>
+            <div className="lang-icons">
+              {props.language}
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
