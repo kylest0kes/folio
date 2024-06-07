1c697fe262ebe8ec2e5582f44e3b623ff9146f49
kylest0kes <kylestokes1551@gmail.com>
2024-06-07 14:26:00 -0400

    whole lot done. project card made, adjustments to layout css made, and moar

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -3,7 +3,18 @@ import "./ProjectCard.css";
 
 const ProjectCard = () => {
   return (
-    <div>FUCK THIS SHIT</div>
+    <div class="card-container">
+    <div class="project-card">
+      <div class="card-project-preview">
+      </div>
+      <div class ="card-project-content">
+        <h4 class="card-project-title">Project Title</h4>
+        <p class="card-project-description">Lorem ipsum dolor sit amet, consetetur sadips
+        elitr, sed diam nonumy eirmod.</p>
+        <button class="project-btn">View case</button>
+      </div>
+    </div>
+  </div>
   );
 };
 
--- src/Sections/Projects/Projects.js
+++ src/Sections/Projects/Projects.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './Projects.css';
+import ProjectCard from '../../Components/ProjectCard/ProjectCard';
 
 const Projects = () => {
   return (
     <div id='projects-section'>
         <p className="projects-header">3. Projects...</p>
+        <div className='project-cards-filter'>
+
+        </div>
+        <div className='project-cards'>
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+          <ProjectCard className='project-card'/>          
+        </div>
     </div>
   )
 }
