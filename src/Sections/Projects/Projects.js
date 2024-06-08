3cbd798c556e266f6c771a4d1daf9d4af8f264fb
kylest0kes <kylestokes1551@gmail.com>
2024-06-08 15:34:10 -0400

    created a filter with filter buttons for the projects section

--- src/Sections/Projects/Projects.js
+++ src/Sections/Projects/Projects.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Projects.css';
 import ProjectCard from '../../Components/ProjectCard/ProjectCard';
 import PROJECTS from "../../Assets/projects/projects";
 
 const Projects = () => {
+  const [filter, setFilter] = useState('');
+
+  const handleSetFilter = (language) => {
+    setFilter(language);
+  }
+
+  const filterProjects = filter ? PROJECTS.filter(project => project.language === filter) : PROJECTS;
+
   return (
     <div id='projects-section'>
         <p className="projects-header">3. Projects...</p>
         <div className='projects-subsection'>
           <div className='project-cards-filter'>
-
+            <button onClick={() => handleSetFilter('')} className='filter-options'>All</button>
+            <button onClick={() => handleSetFilter('Javascript')} className='filter-options'>Javascript</button>
+            <button onClick={() => handleSetFilter('React')} className='filter-options'>React</button>
+            <button onClick={() => handleSetFilter('Python')} className='filter-options'>Python</button>
           </div>
           <div className='project-cards'>
-            {PROJECTS.map((project) => {
+            {filterProjects.map((project) => {
               return (
                 <ProjectCard className='project-card'
                   key={project.id}
