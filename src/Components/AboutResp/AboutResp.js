03900b72d428fa27830801dbb82085d7df0d5f44
kylest0kes <kylestokes1551@gmail.com>
2024-12-15 19:53:07 -0500

    Typo

--- src/Components/AboutResp/AboutResp.js
+++ src/Components/AboutResp/AboutResp.js
@@ -13,7 +13,7 @@ const AboutResp = () => {
 
       <div className="bio-card-text-resp">
         <span className="about-me-desc-resp">
-          <span className="hi">Hi!</span> My name is Kyle, and I am a proficient Full Stack Developer with three years of expertise in the FinTech industry. I specialize in creating seamless, high-quality digital experiences for high-profile clients and their customers.
+          <span className="hi">Hi!</span> My name is Kyle, and I am a proficient Full Stack Developer with three years of expertise in the fintech industry. I specialize in creating seamless, high-quality digital experiences for high-profile clients and their customers.
           <br />
           <br />
           I honed my skills at the University of Central Florida, as well as through various personal projects and extensive coursework on Udemy. Currently, I work at Capco, where I focus on building accessible, dynamic, and inclusive products for various financial clients, leveraging a broad range of technologies to deliver top-tier solutions.
